bf7356408ba27b99f8a11a26cfcfa0645d731ef2
Omer Dogan <omerdogan3@gmail.com>
2018-02-27 22:48:47 +0300

    scrapper v1.0

--- app.js
+++ app.js
@@ -2,7 +2,14 @@ const puppeteer = require('puppeteer');
 const devices = require('puppeteer/DeviceDescriptors');
 const iPhone = devices['iPhone 6'];
 
-let scapeDr = async (category, step) => {
+
+let pageLoader = async() =>{
+	
+}
+
+
+
+let scapeDr = async (category, step, next) => {
   const browser = await puppeteer.launch({headless: false});
   
   const page = await browser.newPage();
@@ -12,11 +19,18 @@ let scapeDr = async (category, step) => {
   
   await page.click(`#ph-topic > div.page-body > div > div > div > div:nth-child(`+ category + `) > a > img`); 
   
-  const findedBook = '#container > div:nth-child(' + step +  ') > div > a.item-name > h3';
+	if(next === true){
+			const nextButton = '#catPageContent > div.container.pager-content > ul > li:nth-child(7) > a.next';
+			await page.waitForSelector(nextButton);
+			await page.click(nextButton);
+			return null;
+	}
+
+  const findedBook = '#container > div:nth-child('+ step + ') > div > a:nth-child(1) > figure > img';
   await page.waitForSelector(findedBook);
   await page.click(findedBook);
-  await page.waitFor(6000);
-
+  await page.waitFor(4000);
+  
 
   const result = await page.evaluate(() => {
     let title = document.querySelector('h1').innerText;
@@ -26,8 +40,8 @@ let scapeDr = async (category, step) => {
     let author = document.querySelector(productDetails + 'div:nth-child(1) > span > a > span').innerText;
     let publisher = document.querySelector(productDetails + 'div:nth-child(2) > h2 > a').innerText;
     let ISBN = document.querySelector(productDetails + " ul > li:nth-child(3)").innerText;
-
-    let summary = document.querySelector("#catPageContent > section.product-details > div.container > div.summary").innerText;
+		let summary = document.querySelector("#catPageContent > section.product-details > div.container > div.summary").innerText;
+		
     return {
         ISBN,
         title,
@@ -38,21 +52,29 @@ let scapeDr = async (category, step) => {
 });
 
   await browser.close();
+  console.log(result.ISBN);
   return result;
 };
 
-// scapeDr().then((value)=>{
-//     console.log(value);
-// });
-
-
-var usingItNow = function(callback) {
-    callback(2,3);
-};
-
-for(var i=2; i<6; ++i){
-    usingItNow(scapeDr);
+let step;
+async function processArray(){
+    for(let category=2; category<23; ++i){
+        let nextPage = false;
+        for(step=83; step<=85; step=step+2){
+            if(step>83){
+                nextPage = true;
+								await scapeDr(category,1,nextPage);
+            }else{
+							await scapeDr(category,step,nextPage);
+						}
+            
+        }
+        
+    }
+    console.log('Done!');
 }
+
+processArray();
 
 
 

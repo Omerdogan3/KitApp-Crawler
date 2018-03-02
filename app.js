7bf7457d7d8817b269ccab6f28458a0a98627940
Omer Dogan <omerdogan3@gmail.com>
2018-03-02 20:23:11 +0300

    mySQL improvement

--- app.js
+++ app.js
@@ -6,20 +6,21 @@ _connection.connect(
     (err) => {
         if (err) throw err;
         console.log("Connected!");
-    }
-);
+});
 
 let step;
 let firstLaunch = true;
 async function processCrawler(){
   for(let category=2; category<=24; category=category+2){
       let nextPage = false;
+      let counter = 1;
       for(step=1; step<=85; step=step+2){
           if(step>83){
               nextPage = true;
               await scapeDr(firstLaunch,category,1,nextPage);
           }else{
-            let bookJson = await scapeDr(firstLaunch,category, step,nextPage);
+            ++counter;
+            let bookJson = await scapeDr(firstLaunch,category, step,nextPage, (counter%3 == 0) ? true : false);
 
             // bookJson.ISBN = bookJson.ISBN.slice(5);
             // var sql = `INSERT INTO books (ISBN, title, author, stars, publisher, summary) VALUES (${parseInt(bookJson.ISBN)}
@@ -28,12 +29,14 @@ async function processCrawler(){
             //     ,${String(bookJson.stars)}
             //     ,${String(bookJson.publisher)}
             //     ,'test')`;
-
+            
             if(bookJson != null){
-                var sql = await util.format('insert into books (ISBN, title, author, stars, publisher, summary) set ("%d","%s","%s","%s","%s","%s")'
-                , bookJson.ISBN.slice(6), bookJson.title, bookJson.author, bookJson.stars.charAt(0), bookJson.publisher, bookJson.summary);
+                var tmp = bookJson.ISBN;
+                var sql = util.format('insert into test (ISBN, title, author, stars, publisher, summary) values ("%d","%s","%s","%d","%s","%s")'
+                , bookJson.ISBN.slice(6) , bookJson.title, bookJson.author, bookJson.stars.charAt(0), bookJson.publisher, 'bookJson.summary');
 
                 await _connection.query(sql , (err,result)=>{
+                    if (err) throw err;
                     // console.log('Inserted');
                     // console.log(result.message);
                 });    
--- drCrawler.js
+++ drCrawler.js
@@ -1,19 +1,28 @@
 const puppeteer = require('puppeteer');
+
 const devices = require('puppeteer/DeviceDescriptors');
 const iPhone = devices['iPhone 6'];
+
+var _connection = require('./database/DBconnection');
+var util = require('util');
+
 
 let browser;
 let page;
 
-module.exports = scapeDr = async (firstLaunch, category, step, next) => {
-  
+module.exports = scapeDr = async (firstLaunch, category, step, next, isScroll) => {
+  console.log(isScroll);
   if(firstLaunch){
-      browser = await puppeteer.launch({headless: false,args: [
+      browser = await puppeteer.launch({headless: false, args: [
         `--window-size=${ 400 },${ 800 }`
       ]});
       page = await browser.newPage();
+      
       await page.emulate(iPhone);
       await page.goto("http://www.dr.com.tr/One-Cikan-Kategoriler");
+      page.on('error', msg => { browser.refresh(); });
+      page.on('pageerror', msg => { browser.refresh();  });
+      
       await page.click(`#ph-topic > div.page-body > div > div > div > div:nth-child(`+ category + `) > a > img`); 
   }
 
@@ -46,15 +55,15 @@ module.exports = scapeDr = async (firstLaunch, category, step, next) => {
     let title = document.querySelector('h1').innerText;
 
     const productDetails = "#catPageContent > section.product-details > div.container > div.all-details > div.specs > ";
-
+    
+    if(document.querySelector(productDetails + " ul > li:nth-child(3)")!= null){
+      ISBN = document.querySelector(productDetails + " ul > li:nth-child(3)").innerText;
+    }
     if(document.querySelector(productDetails + 'div:nth-child(1) > span > a > span') != null){
       author = document.querySelector(productDetails + 'div:nth-child(1) > span > a > span').innerText;
     }
     if(document.querySelector(productDetails + 'div:nth-child(2) > h2 > a') != null){
       publisher = document.querySelector(productDetails + 'div:nth-child(2) > h2 > a').innerText;
-    }
-    if(document.querySelector(productDetails + " ul > li:nth-child(3)")!= null){
-      ISBN = document.querySelector(productDetails + " ul > li:nth-child(3)").innerText;
     }
     if(document.querySelector("#catPageContent > section.product-details > div.container > div.head > div:nth-child(2) > span")!= null){
       stars =document.querySelector("#catPageContent > section.product-details > div.container > div.head > div:nth-child(2) > span").innerText;
@@ -73,12 +82,29 @@ module.exports = scapeDr = async (firstLaunch, category, step, next) => {
     }
   });
 
+  
+      
+
   //eger ki datayi alamazsa tekrardan geri gitmesini onlemek icin.
   if(page.url().startsWith('http://www.dr.com.tr/kategori/') === false){
     await page.goBack();
   }
   
-  console.log(result.title);
+  console.log(result.ISBN);
+  // var sql = util.format('insert into books (ISBN, title, author, stars, publisher, summary) values ("%d","%s","%s","%s","%s","%s")'
+  //        , result.ISBN.slice(6), result.title , result.author, result.stars, 'a', 'xc');
+  
+  // // , result.ISBN.slice(6), result.title, result.author, result.stars.charAt(0), result.publisher, result.summary);
+
+  //       await _connection.query(sql , function (err, result, fields) {
+  //         // if any error while executing above query, throw error
+  //         if (err) throw err;
+  //         // if there is no error, you have the result
+  //         // console.log(result);
+  //       });
+
+
+  
   return result;
 };
 

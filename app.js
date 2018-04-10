ca8a86f0875b1e5e360fd55a59afc12504b051c6
Omer Dogan <omerdogan3@gmail.com>
2018-04-10 19:57:39 +0300

    database Settings

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var util = require('util');
 
 var ISBNCrawler = require('./crawlers/ISBNCrawler');
 
-function handleDisconnect() {
+handleDisconnect = () => {
     _connection.connect(function(err) {              // The server is either down
         if(err) {                                     // or restarting (takes a while sometimes).
         console.log('error when connecting to db:', err);
@@ -12,11 +12,11 @@ function handleDisconnect() {
     });                                     // process asynchronous requests in the meantime.
                                           // If you're also serving http, display a 503 error.
 _connection.on('error', function(err) {
-    console.log('db error', err);
-    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+    // console.log('db error', err);
+    if(err) { // Connection to the MySQL server is usually
       handleDisconnect();                         // lost due to either server restart, or a
-    } else {                                      // connnection idle timeout (the wait_timeout
-      throw err;                                  // server variable configures this)
+    }else{
+        throw err;
     }
     });
 }
--- config/mySqlInfo.js
+++ config/mySqlInfo.js
@@ -1,8 +1,8 @@
 // module.exports = {
 //     host: '94.73.146.81',
 //     database: 'u7832868_kitApp',
-//     user: 'u7832868_Omerdogan',
-//     password: 'Omerdogan.1'
+//     user: 'u7832868_userB35',
+//     password: 'WZcf01K1'
 // };
 
 module.exports = {
